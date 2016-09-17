bebd6cfa3c5d3cd8053fdc5c70ccc5062a2441df
aryshin2016 <aryshin2016@aryshin2016deMacBook-Pro.local>
2016-09-17 22:50:46 +0800

    init my project

---
+++ BaiSiJie/BaiSiJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiJie
+//
+//  Created by aryshin2016 on 16/9/17.
+//  Copyright © 2016年 aryshin2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
